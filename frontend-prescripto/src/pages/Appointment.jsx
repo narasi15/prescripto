eb2ab8515514de93105403528b41963e581889bb
Priyanka <priya94@live.ca>
2025-04-30 19:46:42 -0400

    Updated About page of the website

--- frontend-prescripto/src/pages/Appointment.jsx
+++ frontend-prescripto/src/pages/Appointment.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
 import { assets } from '../assets/assets'
+import RelatedDoctors from '../components/RelatedDoctors'
 
 const Appointment = () => {
 
@@ -124,14 +125,21 @@ const Appointment = () => {
 
         <div className='flex items-center gap-3 w-full overflow-x-scroll mt-4'>
           {docSlots.length && docSlots[slotIndex].map((item, index) => (
-            <p className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer`} key={index}>
+            <p onClick={() => setSlotTime(item.time)} className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer ${item.time === slotTime ? 'bg-blue-400 text-white' : 'text-gray-400 border border-gray-300'}`} key={index}>
               {item.time.toLowerCase()}
             </p>
 
           ))}
         </div>
 
+        <button className='bg-blue-500 text-white text-sm font-light px-14 py-3 rounded-full my-6'>
+          Book An Appointment
+        </button>
+
       </div>
+
+      {/* -- Related Doctors -- */}
+      <RelatedDoctors doc={docId} speciality={docInfo.speciality}/>
       
     </div>
   )
