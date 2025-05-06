e4aca07ac5d7bebbf27eae8989d3c6518b591f67
Priyanka <priya94@live.ca>
2025-05-05 21:43:07 -0400

    Started all the appointment page

--- frontend-prescripto/src/pages/MyAppointments.jsx
+++ frontend-prescripto/src/pages/MyAppointments.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
 
 const MyAppointments = () => {
+
+  const { doctors } = useContext(AppContext)
+
   return (
     <div>
+      <p>My Appointments</p>
+      <div>
+        {doctors.slice(0,2).map((item, index) => (
+          <div key={index}>
+            <div>
+              <img src={item.image} alt=""/>
+            </div>
+
+            <div>
+              <p>{item.name}</p>
+              <p>{item.speciality}</p>
+              <p>Address:</p>
+              <p>{item.address.line1}</p>
+              <p>{item.address.line2}</p>
+              <p><span>Date & Time:</span> 25, July, 2024 | 8:30 PM</p>
+            </div>
+            <div></div>
+            <div>
+              <button>Pay Online</button>
+              <button>Cancel Appointment</button>
+            </div>
+
+          </div>
+
+        ))}
+
+      </div>
       
     </div>
   )
